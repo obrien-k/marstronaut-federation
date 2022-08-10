3dfc1f39633819bc7c48668921eb815017f9196c
Kyle OBrien <obrienk@webbhost.net>
2022-08-10 09:45:23 -0500

    add listen port env var

--- server/index.js
+++ server/index.js
@@ -41,6 +41,6 @@ const server = new ApolloServer({
 
 
 
-server.listen().then(({ url }) => {
+server.listen({port: process.env.PORT || 4000}).then(({ url }) => {
   console.log(`🚀 Gateway ready at ${url}`);
 }).catch(err => {console.error(err)});

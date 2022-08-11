ab2871aa72068fc8bab25fa94fd54610c503f3a9
Kyle OBrien <obrienk@webbhost.net>
2022-08-11 15:08:38 -0500

    deployment changes

--- server/subgraph-nasa/datasources/NasaApi.js
+++ server/subgraph-nasa/datasources/NasaApi.js
@@ -3,7 +3,7 @@ require('dotenv').config({path: '../.env'});
 
 class NasaAPI extends RESTDataSource {
   willSendRequest(request) {
-    request.params.set('api_key',`${process.env.NASA_KEY}`);
+    request.params.set('api_key', process.env.NASA_KEY);
   }
 
   constructor() {

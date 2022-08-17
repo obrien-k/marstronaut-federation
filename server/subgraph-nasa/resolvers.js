9c8f7e97e06ad456f2f812965ee2a3c14c3e086c
Mitch Alderson <mitchell@apollographql.com>
2022-08-17 13:47:27 -0700

    refactor federation entity

--- server/subgraph-nasa/resolvers.js
+++ server/subgraph-nasa/resolvers.js
@@ -1,22 +1,16 @@
 const resolvers = {
   Query: {
     // returns mission info based on the rover name
-    Manifest: (_, {name}, {dataSources}) => {
+    Manifest: (_, { name }, { dataSources }) => {
       return dataSources.nasaAPI.getManifest(name);
     },
-    Photolist: (_, {rover_name, earth_date}, {dataSources}) => {
+    Photolist: (_, { rover_name, earth_date }, { dataSources }) => {
       return dataSources.nasaAPI.getPhotos(rover_name, earth_date);
     },
-    Apod: (_, __, {dataSources}) => {
+    Apod: (_, __, { dataSources }) => {
       return dataSources.nasaAPI.getApod();
-    }
+    },
   },
-  Photo_Details: {
-    totally: () => {
-      return {id: 141592};
-    }
-  }
-
 };
 
 module.exports = resolvers;

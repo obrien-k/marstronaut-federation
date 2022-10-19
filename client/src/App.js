50d10c07e13002d17ede2ef3bef7ea21c8fc712e
Kyle OBrien <obrienk@webbhost.net>
2022-10-19 12:52:28 -0500

    Removed embeddedExplorer, Docker works everywhere but fly

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import XkcdData from './components/xkcd';
-import { EmbeddedExplorer } from './components/embeddedExplorer';
 
 export default function App() {
   return (
     <div>
       <XkcdData />
-      <EmbeddedExplorer />
     </div>
   );
 }
